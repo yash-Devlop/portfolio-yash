560f2b6765236a6438f8ee7d46a2557d4aadafca
yash-Devlop <yash243mac@gmail.com>
2025-01-21 23:10:52 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1200px', // Change lg breakpoint to 1200px
+      xl: '1280px',
+    },
     extend: {
       width: {
         '55p': '55%',
         '45p':'45%',
         '30p': '30%;',
+        '80p': '80%;',
         '800p':'800px'
       },
       height:{
